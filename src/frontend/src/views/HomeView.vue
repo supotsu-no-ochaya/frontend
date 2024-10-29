d17e7edc06a271e8c678a9a33f84bca996304d8c
roriwa <103991412+roriwa@users.noreply.github.com>
2024-10-29 17:45:35 +0100

    updated favicon and added logo

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <main class="min-h-screen grid place-content-center text-2xl">
-    Hello World
+    <img class="size-20 mx-auto" src="/icon.svg" alt="icon" />
+    <div>Hello World</div>
   </main>
 </template>

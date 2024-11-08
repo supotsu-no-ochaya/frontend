859f10ec6db647b6464c70caa01d2e9e882b3c06
roriwa <103991412+roriwa@users.noreply.github.com>
2024-11-08 15:45:34 +0100

    added docker-compose to start the backend and added proxy-configuration to vite.config.ts

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -8,17 +8,22 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  },
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
     },
   },
-  plugins: [
-    vue(),
-  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })

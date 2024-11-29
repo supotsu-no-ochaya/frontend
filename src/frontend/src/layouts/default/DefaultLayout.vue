ec309dae7e70910b3c6d5abb9ca9861c2199a659
roriwa <103991412+roriwa@users.noreply.github.com>
2024-11-29 12:02:50 +0100

    added navigation footer

--- src/frontend/src/layouts/default/DefaultLayout.vue
+++ src/frontend/src/layouts/default/DefaultLayout.vue
@@ -1,15 +1,45 @@
 <script setup lang="ts">
 import type {HTMLAttributes} from "vue";
 import logoSrc from "@/assets/logo.png";
+import type {RouteLocationRaw} from "vue-router";
+
+import billSrc from "@/assets/icons/navigation/bill.svg";
+import homeSrc from "@/assets/icons/navigation/home.svg";
+import shoppingCartSrc from "@/assets/icons/navigation/shopping-cart.svg";
 
 const props = defineProps<{
   class?: HTMLAttributes['class']
+  showNavigationFooter?: boolean
 }>();
+
+interface FooterLink {
+  iconSrc: string
+  alt: string
+  location: RouteLocationRaw
+}
+
+const footerLinks: FooterLink[] = [
+  {
+    iconSrc: homeSrc,
+    alt: "Home",
+    location: {  },
+  },
+  {
+    iconSrc: billSrc,
+    alt: "Bill",
+    location: {  },
+  },
+  {
+    iconSrc: shoppingCartSrc,
+    alt: "Shopping Cart",
+    location: {  },
+  },
+]
 </script>
 
 <template>
   <div class="min-h-screen grid grid-rows-[auto,1fr,auto]">
-    <header class="flex items-center p-2 gap-x-2 bg-gradient-to-b from-primary via-primary to-background">
+    <header class="flex items-center p-2 gap-x-2 bg-gradient-to-b from-primary via-primary to-transparent">
       <img :src="logoSrc" alt="logo" class="block rounded-full size-12" />
       <p>
         Supotso no Ochaya
@@ -19,8 +49,14 @@ const props = defineProps<{
     <main :class="props.class">
       <slot />
     </main>
-    <footer>
-
+    <footer class="border-t">
+      <div v-if="showNavigationFooter" class="flex gap-2 justify-around items-baseline p-2">
+        <template v-for="link of footerLinks" :key="link">
+          <router-link :to="link.location" :title="link.alt" class="p-2">
+            <img :src="link.iconSrc" :alt="link.alt" class="size-8" />
+          </router-link>
+        </template>
+      </div>
     </footer>
   </div>
 </template>

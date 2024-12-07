578acdabe3132251a204c6dee616d05adee6d6ac
roriwa <103991412+roriwa@users.noreply.github.com>
2024-12-07 13:33:32 +0100

    updated auth page layout

--- src/frontend/src/pages/auth/login.vue
+++ src/frontend/src/pages/auth/login.vue
@@ -5,6 +5,10 @@ import { Button } from "@/components/ui/button";
 import { ref } from "vue";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { LucideCircleAlert } from "lucide-vue-next";
+import { useRouter } from "vue-router";
+import { Label } from "@/components/ui/label";
+
+const router = useRouter();
 
 const emailValue = ref("");
 const passwordValue = ref("");
@@ -13,21 +17,22 @@ const loginFailed = ref(false);
 function doLogin() {
   loginFailed.value = false;
   alert(`Doing Login with ${ emailValue.value } and ${ passwordValue.value }.`);
+  router.push({ name: "/" });
 }
 </script>
 
 <template>
   <DefaultLayout class="p-4 grid place-content-center">
-    <form class="w-full max-w-lg flex flex-col gap-4" @submit.prevent="doLogin">
-      <div class="flex flex-col gap-0.5">
-        <label for="email">Email</label>
-        <Input v-model.trim="emailValue" id="email" type="email" />
-        <span class="text-muted-foreground">Geben Sie ihre Email-Adresse ein</span>
+    <form class="max-w-sm space-y-4" @submit.prevent="doLogin">
+      <div class="space-y-0.5">
+        <Label for="email">Email</Label>
+        <Input v-model.trim="emailValue" id="email" type="email" required />
+        <span class="text-muted-foreground text-sm">Geben Sie ihre Email-Adresse ein</span>
       </div>
-      <div class="flex flex-col gap-0.5">
-        <label for="password">Passwort</label>
-        <Input v-model.trim="passwordValue" id="password" type="password" />
-        <span class="text-muted-foreground">Geben Sie ihr Password ein</span>
+      <div class="space-y-0.5">
+        <Label for="password">Passwort</Label>
+        <Input v-model.trim="passwordValue" id="password" type="password" required />
+        <span class="text-muted-foreground text-sm">Geben Sie ihr Password ein</span>
       </div>
       <Alert v-if="loginFailed" variant="destructive">
         <LucideCircleAlert class="size-4" />
@@ -38,7 +43,7 @@ function doLogin() {
           Die Email-Adresse oder das Passwort ist falsch
         </AlertDescription>
       </Alert>
-      <Button type="submit">Einloggen</Button>
+      <Button type="submit" class="w-full">Einloggen</Button>
     </form>
   </DefaultLayout>
 </template>

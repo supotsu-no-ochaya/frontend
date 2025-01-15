a85bc9c0e880d573379221b6da32483ed6524666
roriwa <103991412+roriwa@users.noreply.github.com>
2025-01-15 19:21:36 +0100

    added functionality to login and logout pages

--- src/frontend/src/pages/auth/logout.vue
+++ src/frontend/src/pages/auth/logout.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+import { authService } from "@/services/user/authService.ts";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
+
+await authService.logout();
+await router.push("/");
 </script>
 
 <template>

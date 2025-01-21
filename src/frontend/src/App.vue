337b484fa13e537aa8ff612592e779fb47897121
roriwa <103991412+roriwa@users.noreply.github.com>
2025-01-21 19:40:59 +0100

    improved App.vue router-view

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
 import {Toaster} from "@/components/ui/toast";
 import { LucideLoaderCircle } from "lucide-vue-next";
-
 </script>
 
 <template>
-  <Suspense>
-    <RouterView />
-    <template #fallback>
-      <div class="min-h-screen p-4 grid place-items-center">
-        <LucideLoaderCircle class="size-10 animate-spin" />
-      </div>
-    </template>
-  </Suspense>
+  <router-view v-slot="{ Component }">
+    <Suspense>
+      <component :is="Component" :key="$route.fullPath" />
+      <template #fallback>
+        <div class="min-h-screen p-4 grid place-items-center">
+          <LucideLoaderCircle class="size-10 animate-spin" />
+        </div>
+      </template>
+    </Suspense>
+  </router-view>
   <Toaster />
 </template>

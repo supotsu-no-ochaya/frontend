c3c2de5b5b39934ab6875da4c8409a0431663e8f
Philsie <psiegerist.gaming@gmail.com>
2025-01-23 21:51:10 +0100

    fixed bug with accordion and updated displayed cost

--- src/frontend/src/pages/waiter/table/[tableId]/payment.vue
+++ src/frontend/src/pages/waiter/table/[tableId]/payment.vue
@@ -127,7 +127,9 @@ function updateTotalSum(){
 function updateOrderTotal(order){
   const id = "order-"+order.id
 
-  document.getElementById(id).textContent = order.id+': '+order.total.toFixed(2)+'€';
+  const element = document.getElementById(id);
+  if (element !== null)
+    element.textContent = order.id+': '+(order.total/100).toFixed(2)+'€';
 }
 
 </script>
@@ -155,7 +157,6 @@ function updateOrderTotal(order){
                     <div>{{menuItems.find(menuItem => menuItem.id === orderItem.menu_item).name}}</div>
                   </TableCell>
                   <TableCell class="w-2/5" v-if="orderItem.order == order.id" >
-                    Test
                     <div>{{ orderItem.price/100 }}€</div>
                   </TableCell>
                   <TableCell class="w-1/5" v-if="orderItem.order == order.id">
@@ -167,7 +168,7 @@ function updateOrderTotal(order){
                 </TableRow>
               </TableBody>
             </Table>
-            <div class="flex items-center justify-center font-bold mt-2" :id="'order-' + order.id">{{ order.id }}: 0€</div>
+            <div class="flex items-center justify-center font-bold mt-2" :id="'order-' + order.id">{{ order.id }}: 0.00€</div>
           </AccordionContent>
         </AccordionItem>
       </Accordion>

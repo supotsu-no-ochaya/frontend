b0cbb76e2e507cd9b22ddf0534e9327e535a5a22
Philsie <psiegerist.gaming@gmail.com>
2025-01-28 22:36:07 +0100

    added Timer function to tables

--- src/frontend/src/components/TableInfo.js
+++ src/frontend/src/components/TableInfo.js
@@ -2,23 +2,28 @@ import { defineStore } from 'pinia';
 import { useLocalStorage } from '@vueuse/core';
 
 export const useTableStore = defineStore('table', () => {
-  const table = useLocalStorage('table', {}); // Reactive localStorage-backed cart
+  const table = useLocalStorage('table', {
+    persons: {}, // For counting people by ID
+    timers: {} // To store additional properties or data
+  });
 
   const addPerson = (id) => {
-  	if (id in table.value){
-		table.value[id] += 1
-  	} else {
-  		table.value[id] = 1
-  	}
-  }
+    if (id in table.value.persons) {
+      table.value.persons[id] += 1; // Increment count for this ID
+    } else {
+      table.value.persons[id] = 1; // Initialize count for this ID
+    }
+  };
 
   const removePerson = (id) => {
-  	if (id in table.value && table.value[id] > 0){
-  		table.value[id] -= 1
-  	} else {
-  		table.value[id] = 1
-  	}
-  }
+    if (id in table.value.persons && table.value.persons[id] > 1) {
+      table.value.persons[id] -= 1; // Decrement count
+    } else if (id in table.value.persons) {
+      delete table.value.persons[id]; // Remove the ID when count reaches zero
+    }
+  };
+
+
 
   return { table, addPerson, removePerson};
 });
